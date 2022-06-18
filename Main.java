bbe9ef333fb012e226a3b032ed3315c33840f59f
toshinobu0427 <dfnar405@gmail.com>
2022-06-19 02:33:42 +0900

    finish 3rd

--- Main.java
+++ Main.java
@@ -275,3 +275,248 @@
 //
 //	}
 //}
+
+//3-1
+//public class Main {
+//	public static void main(String[] args) {
+//		boolean tenki = true;
+//		if (tenki == true) {
+//			System.out.println("洗濯をします");
+//			System.out.println("散歩にいきます");
+//		} else {
+//			System.out.println("DVDを観ます");
+//		}
+//
+//	}
+//}
+
+//3-2
+//public class Main {
+//	public static void main(String[] args) {
+//		boolean doorClose = true; // ここでtrueかfalseを代入
+//		while (doorClose == true) {
+//			System.out.println("ノックする");
+//			System.out.println("1分待つ");
+//		}
+//
+//	}
+//}
+
+//3-3　非推奨
+//public class Main {
+//	public static void main(String[] args) {
+//		boolean tenki = true;
+//		if (tenki == true) {
+//			System.out.println("洗濯をします");
+//			System.out.println("散歩にいきます");
+//		} else
+//			System.out.println("DVDを観ます");
+//
+//	}
+//}
+
+//3-4
+//public class Main {
+//	public static void main(String[] args) {
+//
+//		System.out.println("あなたの運勢を占います");
+//
+//		int fortune = new java.util.Random().nextInt(4) + 1;
+//
+//		if (fortune == 1) {
+//			System.out.println("大吉");
+//
+//		} else if (fortune == 2) {
+//			System.out.println("中吉");
+//
+//		} else if (fortune == 3) {
+//			System.out.println("吉");
+//
+//		} else {
+//			System.out.println("凶");
+//		}
+//	}
+//}
+
+//3-5
+//public class Main {
+//	public static void main(String[] args) {
+//		System.out.println("あなたの運勢を占います");
+//		int fortune = 1;
+//		switch (fortune) {
+//		case 1:
+//			System.out.println("大吉");
+//
+//		case 2:
+//			System.out.println("中吉");
+//			break;
+//		case 3:
+//			System.out.println("吉");
+//			break;
+//		default:
+//			System.out.println("凶");
+//
+//		}
+//
+//	}
+//}
+
+//3-6
+//public class Main {
+//	public static void main(String[] args) {
+//		System.out.println("あなたの運勢を占います");
+//		int fortune = new java.util.Random().nextInt(5) + 1;
+//		switch (fortune) {
+//		case 1:
+//
+//		case 2:
+//			System.out.println("いいね！");
+//			break;
+//
+//		case 3:
+//			System.out.println("普通です");
+//			break;
+//		case 4:
+//		case 5:
+//			System.out.println("うーん・・・");
+//
+//		}
+//
+//	}
+//}
+
+//3-7
+//public class Main {
+//	public static void main(String[] args) {
+//		
+//		for (int i = 0; i < 10; i++) {
+//			System.out.println("こんにちは");
+//
+//		}
+//
+//	}
+//}
+
+//3-8
+//public class Main {
+//	public static void main(String[] args) {
+//
+//		for (int i = 0; i < 3; i++) {
+//			System.out.print("現在" + (i + 1) + "周目→");
+//
+//		}
+//
+//	}
+//}
+
+//3-9
+//public class Main {
+//	public static void main(String[] args) {
+//		for (int i = 1; i < 10; i++) {
+//			for (int j = 1; j < 10; j++) {
+//				System.out.print(i * j);
+//				System.out.print(" ");
+//			}
+//			System.out.println("");
+//		}
+//
+//	}
+//}
+
+//exe3-1
+//weight == 60;
+//(age1 + age2) * 2 > 60;
+//age % 2 == 1;
+//name.equals("湊");
+
+//exe3-2
+//c,d,e
+
+//exe3-3
+//public class Main {
+//	public static void main(String[] args) {
+//		int isHungry = 1;
+//		String food = "ラーメン";
+//		System.out.println("こんにちは");
+//
+//		if (isHungry == 0) {
+//			System.out.println("お腹がいっぱいです");
+//
+//		} else {
+//			System.out.println("はらぺこです");
+//
+//		}
+//		if (isHungry == 1) {
+//			System.out.println(food + "をいただきます");
+//		}
+//		System.out.println("ごちそうさまでした");
+//	}
+//}
+
+//exe3-4
+//public class Main {
+//	public static void main(String[] args) {
+//		boolean tenki = false;
+//		if (tenki == true) {
+//			System.out.println("洗濯をします");
+//			System.out.println("散歩に行きます");
+//
+//		} else {
+//			System.out.println("DVDを観ます");
+//			System.out.println("寝ます");
+//		}
+//	}
+//}
+
+//exe3-5
+//public class Main {
+//	public static void main(String[] args) {
+//		System.out.print("[メニュー]　1:検索　2:登録　3:削除　4:変更>");
+//		int selected = new java.util.Scanner(System.in).nextInt();
+//
+//		switch (selected) {
+//		case 1:
+//			System.out.println("検索します");
+//			break;
+//
+//		case 2:
+//			System.out.println("登録します");
+//			break;
+//
+//		case 3:
+//			System.out.println("削除します");
+//			break;
+//		case 4:
+//			System.out.println("変更します");
+//			break;
+//
+//		}
+//
+//	}
+//}
+
+//exe3-6
+//public class Main {
+//	public static void main(String[] args) {
+//		System.out.println("【数あてゲーム】");
+//
+//		int ans = new java.util.Random().nextInt(10);
+//
+//		for (int i = 0; i < 5; i++) {
+//
+//			System.out.println("0～9の数字を入力してください");
+//
+//			int num = new java.util.Scanner(System.in).nextInt();
+//
+//			if (num == ans) {
+//				System.out.println("アタリ！");
+//				break;
+//
+//			} else {
+//				System.out.println("違います");
+//			}
+//
+//		}
+//		System.out.println("ゲームを終了します");
+//	}
+//}
