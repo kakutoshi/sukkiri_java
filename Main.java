9e6a1f3c62f0c196590ad2993d2b88b11fb7c158
toshinobu0427 <dfnar405@gmail.com>
2022-06-18 23:03:30 +0900

    first commit

---
+++ Main.java
@@ -0,0 +1,88 @@
+//0-1
+//public class Main {
+//	public static void main(String[] args) {
+//		System.out.println("Hello World");
+//	}
+//}
+//0-2
+//public class Main {
+//	public static void main(String[] args) {
+//		System.out.println("すがわら");
+//	}
+//}
+//0-3
+//public class Main {
+//	public static void main(String[] args) {
+//
+//		System.out.println("すがわら");
+//		System.out.println("31才です");
+//		System.out.println("お酒が好きです");
+//	}
+//}
+//0-5
+//public class Main {
+//	public static void main(String[] args) {
+//		System.out.println("すがわら");
+//		System.out.println("31才です");
+//		System.out.println("お酒が好きです");
+//		System.out.println("31+31計算をします");
+//		System.out.println(31 + 31);
+//	}
+//}
+//0-6
+//public class Main {
+//	public static void main(String[] args) {
+//		int x;
+//		x = 6;
+//		System.out.println(x * x * 3.14);
+//
+//	}
+//}
+//1-2
+//public class Main {
+//	public static void main(String[] args) {
+//		int age;
+//		age = 30;
+//		System.out.println(age);
+//
+//	}
+//}
+//1-3
+//public class Main {
+//	public static void main(String[] args) {
+//		int age = 20;
+//		System.out.println("私の年齢は" + age);
+//		age = 31;
+//		System.out.println("・・・いや、本当の年齢は" + age);
+//	}
+//}
+
+//1-5
+//public class Main {
+//	public static void main(String[] args) {
+//		final double PI = 3.14;
+//		int pie = 5;
+//		System.out.println("半径" + pie + "㎝のパイの面積は、");
+//		System.out.println(pie * pie * PI);
+//		System.out.println("パイの半径を倍にします");
+//		PI = 10;
+//		System.out.println("半径" + pie + "㎝のパイの面積は、");
+//		System.out.println(pie * pie * PI);
+//
+//	}
+//}
+//exe1-2
+//public class Main {
+//	public static void main(String[] args) {
+//		int a = 3;
+//		int b = 5;
+//		int c = a * b;
+//		System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + c);
+//	}
+//}
+//exe1-3
+//boolean result = true;
+//char favoriteCharacter = '駆';
+//double pi = 3.14;
+//long number = 314159265853979L;
+//String msg = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
