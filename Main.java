da806d34aac855181d026411e278813fbda4d27f
toshinobu0427 <dfnar405@gmail.com>
2022-06-19 13:26:39 +0900

    finished 4th

--- Main.java
+++ Main.java
@@ -520,3 +520,326 @@
 //		System.out.println("ゲームを終了します");
 //	}
 //}
+
+//4-1
+//public class Main {
+//	public static void main(String[] args) {
+//		int sansu = 20;
+//		int kokugo = 30;
+//		int rika = 40;
+//		int eigo = 50;
+//		int syakai = 80;
+//
+//		int sum = sansu + kokugo + rika + eigo + syakai;
+//
+//		int avg = sum / 5;
+//
+//		System.out.println("合計点:" + sum);
+//		System.out.println("平均点:" + avg);
+//
+//	}
+//}
+
+//4-2
+//public class Main {
+//	public static void main(String[] args) {
+//		int[] scores;
+//		scores = new int[5];
+//
+//	}
+//}
+
+//4-3
+//public class Main {
+//	public static void main(String[] args) {
+//		int[] scores = new int[5];
+//
+//	}
+//}
+
+//4-4
+//public class Main {
+//	public static void main(String[] args) {
+//		int[] scores = new int[5];
+//		int num = scores.length;
+//		System.out.println("要素の数:" + num);
+//
+//	}
+//}
+
+//4-5
+//public class Main {
+//	public static void main(String[] args) {
+//		int[] scores;
+//		scores = new int[5];
+//		scores[1] = 30;
+//		System.out.println(scores[1]);
+//
+//	}
+//}
+
+//4-6
+//public class Main {
+//	public static void main(String[] args) {
+//		int x;
+//		System.out.println(x);
+//
+//	}
+//}
+
+//4-7
+//public class Main {
+//	public static void main(String[] args) {
+//		int[] scores = new int[5];
+//		System.out.println(scores[0]);
+//
+//	}
+//}
+
+//4-8
+//public class Main {
+//	public static void main(String[] args) {
+//		int[] scores = { 20, 30, 40, 50, 80 };
+//		int sum = scores[1] + scores[2] + scores[3] + scores[4] + scores[5];
+//		int avg = sum / scores.length;
+//		System.out.println("合計点:" + sum);
+//		System.out.println("平均点:" + avg);
+//
+//	}
+//}
+
+//4-9
+//public class Main {
+//	public static void main(String[] args) {
+//		int[] scores = { 20, 30, 40, 50, 80 };
+//		for (int i = 0; i < scores.length; i++) {
+//			System.out.println(scores[i]);
+//		}
+//
+//	}
+//}
+
+//4-10
+//public class Main {
+//	public static void main(String[] args) {
+//		int[] scores = { 20, 30, 40, 50, 80 };
+//		int sum = 0;
+//		for (int i = 0; i < scores.length; i++) {
+//			sum += scores[i];
+//		}
+//
+//		int avg = sum / scores.length;
+//		System.out.println("合計点:" + sum);
+//		System.out.println("平均点:" + avg);
+//
+//	}
+//}
+
+//要素の数を増やして検証
+//public class Main {
+//	public static void main(String[] args) {
+//		int[] scores = { 20, 30, 40, 50, 80, 100, 100, 100, 100 };
+//		int sum = 0;
+//		for (int i = 0; i < scores.length; i++) {
+//			sum += scores[i];
+//		}
+//
+//		int avg = sum / scores.length;
+//		System.out.println("合計点:" + sum);
+//		System.out.println("平均点:" + avg);
+//
+//	}
+//}
+
+//4-11
+//public class Main {
+//	public static void main(String[] args) {
+//		int[] scores = { 20, 30, 40, 50, 80 };
+//		int count = 0;
+//		for (int i = 0; i < scores.length; i++) {
+//			if (scores[i] >= 50) {
+//				count++;
+//			}
+//		}
+//
+//		System.out.println("50点以上の科目の数は:" + count);
+//	}
+//}
+
+//4-12
+//public class Main {
+//	public static void main(String[] args) {
+//		int[] seq = new int[10];
+//
+//		// 塩基配列をランダムに生成
+//		for (int i = 0; i < seq.length; i++) {
+//			seq[i] = new java.util.Random().nextInt(4);
+//		}
+//
+//		// 生成した塩基配列の記号を表示
+//		for (int i = 0; i < seq.length; i++) {
+////			switch (seq[i]) {
+////			case 0:
+////				System.out.print("A");
+////
+////				break;
+////			case 1:
+////				System.out.print("T");
+////
+////				break;
+////			case 2:
+////				System.out.print("G");
+////
+////				break;
+////			case 3:
+////				System.out.print("C");
+////
+////				break;
+////				
+////		
+////
+////			}
+//
+//			char[] base = { 'A', 'T', 'G', 'C' };
+//			System.out.print(base[seq[i]] + " ");
+//		}
+//
+//	}
+//}
+
+//4-13
+//public class Main {
+//	public static void main(String[] args) {
+//		int[] scores = { 20, 30, 40, 50, 80 };
+//
+//		for (int i = 0; i < scores.length; i++) {
+//			System.out.println(scores[i]);
+//		}
+//
+//	}
+//}
+
+//4-14
+//public class Main {
+//	public static void main(String[] args) {
+//		int[] scores = { 20, 30, 40, 50, 80 };
+//
+//		for (int value : scores) {
+//			System.out.println(value);
+//		}
+//
+//	}
+//}
+
+//4-15
+//public class Main {
+//	public static void main(String[] args) {
+//		int[] arrayA = { 1, 2, 3 };
+//		int[] arrayB;
+//		arrayB = arrayA;
+//		arrayB[0] = 100;
+//		System.out.println(arrayA[0]);
+//
+//	}
+//}
+
+//4-16
+//public class Main {
+//	public static void main(String[] args) {
+//		boolean judge = true;
+//		if (judge == true) {
+//			int[] array = { 1, 2, 3 };
+//		}
+//	}
+//}
+
+//4-17
+//public class Main {
+//	public static void main(String[] args) {
+//		int[] array = { 1, 2, 3 };
+//		array = null;
+//		array[0] = 10;
+//
+//	}
+//}
+
+//4-18
+//public class Main {
+//	public static void main(String[] args) {
+//		int[][] scores = new int[2][3];
+//		scores[0][0] = 40;
+//		scores[0][1] = 50;
+//		scores[0][2] = 60;
+//		scores[1][0] = 80;
+//		scores[1][1] = 60;
+//		scores[1][2] = 70;
+//		System.out.println(scores[1][1]);
+//
+//	}
+//}
+
+//4-19
+//public class Main {
+//	public static void main(String[] args) {
+//		int[][] scores = { { 40, 50, 60 }, { 80, 60, 70 } };
+//
+//		System.out.println(scores.length);
+//		System.out.println(scores[0].length);
+//
+//	}
+//}
+
+//exe4-1
+//public class Main {
+//	public static void main(String[] args) {
+//		int[] points = new int[4];
+//		double[] weights = new double[5];
+//		boolean[] answers = new boolean[3];
+//		String[] names = new String[3];
+//
+//	}
+//}
+
+//exe4-2
+//public class Main {
+//	public static void main(String[] args) {
+//		int[] moneyList = { 121902, 8302, 55100 };
+//		for (int i = 0; i < 3; i++) {
+//			System.out.println(moneyList[i]);
+//		}
+//
+//		for (int m : moneyList) {
+//			System.out.println(m);
+//		}
+//
+//	}
+//}
+
+//exe4-3
+//public class Main {
+//	public static void main(String[] args) {
+//		int[] counts = null;
+//		float[] heights = { 171.3F, 175.0F };
+//		System.out.println(counts[1]);
+//		System.out.println(heights[2]);
+//	}
+//}
+
+//java.lang.NullPointerException
+//java.lang.ArrayIndexOutOfBoundsException
+
+//exe4-4
+//public class Main {
+//	public static void main(String[] args) {
+//		int[] numbers = { 3, 4, 9 };
+//		System.out.println("1桁の数字を入力してください");
+//		int input = new java.util.Scanner(System.in).nextInt();
+//
+//		for (int n : numbers) {
+//			if (n == input) {
+//				System.out.println("アタリ！");
+//			}
+//		}
+//
+//	}
+//}
